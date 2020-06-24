5d339dab8cf653a4f9a42fda63c5dad261d8eb32
이은별 <lbj4298@naver.com>
2020-06-24 20:05:41 +0900

    [add]자바기초공부

---
+++ 6th/이은별/0624-study/undefined.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class undefined {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println(scanner.next());
+    }
+}
+
