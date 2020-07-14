8a5456fd49c4439675217d8b90c7a38758160c9b
서건희 <201206sgh@dsm.hs.kr>
2020-07-14 19:44:54 +0900

    [ADD]문자열을 정수형으로 변환하기

---
+++ 6th/서건희/0713_Study/algorithm.java
@@ -0,0 +1,10 @@
+public class algorithm {
+
+    public static int solution(String s){
+        return Integer.parseInt(s);
+    }
+    public static void main(String[] args) {
+        String s = "-1234";
+        System.out.println(solution(s));
+    }
+}
